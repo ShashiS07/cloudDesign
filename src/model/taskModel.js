110198af12e63b63855d920dcb1fdb8b5e1b92ff
ShashiS07 <shashiss0708@gmail.com>
2023-01-22 15:07:40 +0530

    okay

--- src/model/taskModel.js
+++ src/model/taskModel.js
@@ -3,8 +3,7 @@ const mongoose=require('mongoose')
 const taskSchema= new mongoose.Schema({
     title:{type:String},
     discription:{type:String},
-    status:{type:String, enum:["Open","In-Progress","Completed"]},
-    DeletedAt:{type: Date}
+    status:{type:String, enum:["Open","In-Progress","Completed"]}
 },{timestamps:true})
 
 module.exports=mongoose.model('task',taskSchema)
